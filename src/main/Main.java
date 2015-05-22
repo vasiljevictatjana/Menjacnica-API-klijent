497461175c5e82ac5d77ad33b215faa7c0ab3a2c
Tatjana <vasiljevictatjana@yahoo.com>
2015-05-23 00:12:03 +0200

    Napravljena klasa Main

---
+++ src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import util.AzuriranjeKursneListe;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+		AzuriranjeKursneListe azuriraj = new AzuriranjeKursneListe();
+		azuriraj.azurirajValute();
+}
